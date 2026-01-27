83200d06a6795108ea2bc7a0a949d1d2ed5c1dde
Oleksii Lubynets <O.Lubynets@gsi.de>
2026-01-27 09:54:03 +0100

    add deltaPhi and occMedianTime to training_neural_networks/training_qa

--- framework/training_neural_networks/training_qa.py
+++ framework/training_neural_networks/training_qa.py
@@ -39,6 +39,14 @@ HadronicRateBool = "fHadronicRate" in CONFIG['createTrainingDatasetOptions']['la
 if HadronicRateBool:
     LOG.debug("Using Hadronic Rate option")
 
+PhiBool = "fPhi" in CONFIG['createTrainingDatasetOptions']['labels_x']
+if PhiBool:
+    LOG.debug("Using Phi option")
+
+OccMedianTimeBool = "fOccMedianTime" in CONFIG['createTrainingDatasetOptions']['labels_x']
+if OccMedianTimeBool:
+    LOG.debug("Using Occupancy Median Time option")
+
 ### Data preparation
 
 ## Loading data and models
@@ -368,6 +376,21 @@ if HadronicRateBool:
 else:
     LOG.debug("No folder for QA plots for HadronicRate (disabled)")
 
+if PhiBool:
+    os.system("rm -rf " + qa_dir + "/fPhi")
+    os.makedirs(qa_dir + "/fPhi")
+    LOG.debug("Created folder for QA plots for Phi")
+else:
+    LOG.debug("No folder for QA plots for Phi (disabled)")
+
+if OccMedianTimeBool:
+    os.system("rm -rf " + qa_dir + "/fOccMedianTime")
+    os.makedirs(qa_dir + "/fOccMedianTime")
+    LOG.debug("Created folder for QA plots for OccMedianTime")
+else:
+    LOG.debug("No folder for QA plots for OccMedianTime (disabled)")
+
+
 for i, mass in enumerate(np.sort(np.unique(fit_data[:,labels=='fMass'].flatten()))):
 
     def transform_ncl(x):
@@ -415,6 +438,19 @@ for i, mass in enumerate(np.sort(np.unique(fit_data[:,labels=='fMass'].flatten()
         QA2D_NSigma_vs_Var(i, mass, plot_against = 'fHadronicRate', log_x = True, range_hists = [[-1.,0.3]]*6, useNN=False, xlabel = r'Hadronic Rate [50kHz]')
         QA2D_NSigma_vs_Var(i, mass, plot_against = 'fHadronicRate', log_x = True, range_hists = [[-1.,0.3]]*6, plot_mode="rel_sigma",xlabel = r'Hadronic Rate [50kHz]')
         QA2D_NSigma_vs_Var(i, mass, plot_against = 'fHadronicRate', log_x = True, range_hists = [[-1.,0.3]]*6, plot_mode="raw_relsigma_fitted",xlabel = r'Hadronic Rate [50kHz]')
+
+    if(PhiBool):
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fPhi', log_x = False, range_hists = [[-0.03,0.38]]*6, xlabel = r'phi - k*pi/9')
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fPhi', log_x = False, range_hists = [[-0.03,0.38]]*6, useNN=False, xlabel = r'phi - k*pi/9')
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fPhi', log_x = False, range_hists = [[-0.03,0.38]]*6, plot_mode="rel_sigma",xlabel = r'phi - k*pi/9')
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fPhi', log_x = False, range_hists = [[-0.03,0.38]]*6, plot_mode="raw_relsigma_fitted",xlabel = r'phi - k*pi/9')
+
+    if(OccMedianTimeBool):
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fOccMedianTime', log_x = False, range_hists = [[-50,120]]*6, xlabel = r'occ. median time')
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fOccMedianTime', log_x = False, range_hists = [[-50,120]]*6, useNN=False, xlabel = r'occ. median time')
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fOccMedianTime', log_x = False, range_hists = [[-50,120]]*6, plot_mode="rel_sigma",xlabel = r'occ. median time')
+        QA2D_NSigma_vs_Var(i, mass, plot_against = 'fOccMedianTime', log_x = False, range_hists = [[-50,120]]*6, plot_mode="raw_relsigma_fitted",xlabel = r'occ. median time')
+
 
 separation_power(useNN=1, useMassAssumption=0)
 separation_power(useNN=0, useMassAssumption=0)
